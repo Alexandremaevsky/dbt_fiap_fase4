d6ae0a627f23af24025b6aae511934d88efd59a7
Alexandre Maevsky <alexandremaevsky@gmail.com>
2023-08-23 20:46:57 +0000

    alterando joins

--- models/joins.sql
+++ models/joins.sql
@@ -1,9 +1,9 @@
 with calc_table_final as (
 select
 *
-from {{source('sources','calendar')}} "calendar"
-left join {{source('sources','listings')}} "listings" on ("calendar".listing_id = "listings".id)
-left join {{source('sources','neighbourhoods')}} "neighbourhoods" on ("neighbourhoods".neighbourhood_group = "listings".neighbourhood_group)
-left join {{source('sources','reviews')}} "reviews" on ("reviews".listing_id = "listings".id)
+from {{source('sources','calendar')}} cal
+left join {{source('sources','listings')}} lis on (cal.listing_id =lis.id)
+left join {{source('sources','neighbourhoods')}} nei on (nei.neighbourhood_group = lis.neighbourhood_group)
+left join {{source('sources','reviews')}} rev on (rev.listing_id = lis.id)
 )
 select * from calc_table_final
